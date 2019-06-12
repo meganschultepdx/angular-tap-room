40d40487dd37b98405ee0dd8aa7e0ee0cc31e8b6
Student <example@example.com>
2019-06-12 14:34:48 -0700

    install materialize and include parallax feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { KegsComponent } from './kegs/kegs.component';
 import { FormComponent } from './form/form.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import 'materialize-css';
 
 
 @NgModule({
