295fddb00f96cc92cd91797c72fb291cd53e9804
Student <example@example.com>
2019-06-12 09:58:43 -0700

    add keg array - in progress

--- src/app/kegs/kegs.component.ts
+++ src/app/kegs/kegs.component.ts
@@ -1,6 +1,7 @@
 // See what happends when you delete OnInit and ngOnInit
 
 import { Component, OnInit } from '@angular/core';
+import { Keg } from '../models/keg.model';
 
 @Component({
   selector: 'app-kegs',
@@ -9,11 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KegsComponent implements OnInit {
 
-  constructor() {
-
-   }
-
   ngOnInit() {
   }
 
+  kegs: Keg[] = [
+    new Keg('Black Butte', 'Deschutes', 'Porter', 6, 5.2),
+    new Keg('Gimme Mo', 'Crux', 'IPA', 6, 6.2),
+    new Keg('Sacrilege Sour Cherry', 
+    'Reverend Nat\'s', 'Cider', 12, 6.0),
+    new Keg('Ice', 'Milwaukee\'s Best', 'Lager', 2, 3)
+  ]
 }
