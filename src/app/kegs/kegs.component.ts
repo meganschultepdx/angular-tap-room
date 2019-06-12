d148364c4884c2fe35c83dc8bd8808102f1d90bc
Student <example@example.com>
2019-06-12 11:18:37 -0700

    display kegs and details / toggling each individual keg's detail in progress

--- src/app/kegs/kegs.component.ts
+++ src/app/kegs/kegs.component.ts
@@ -2,6 +2,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Keg } from '../models/keg.model';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-kegs',
@@ -16,8 +17,18 @@ export class KegsComponent implements OnInit {
   kegs: Keg[] = [
     new Keg('Black Butte', 'Deschutes', 'Porter', 6, 5.2),
     new Keg('Gimme Mo', 'Crux', 'IPA', 6, 6.2),
-    new Keg('Sacrilege Sour Cherry', 
-    'Reverend Nat\'s', 'Cider', 12, 6.0),
+    new Keg('Sacrilege Sour Cherry',
+      'Reverend Nat\'s', 'Cider', 12, 6.0),
     new Keg('Ice', 'Milwaukee\'s Best', 'Lager', 2, 3)
   ]
+
+  selectedKeg: null;
+
+  toggleDetails(keg) {
+    if (this.selectedKeg === null) {
+      this.selectedKeg = keg
+    } else {
+      this.selectedKeg = null
+    }
+  }
 }
