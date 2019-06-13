a401918407e94c65291d008495b9967009fd0faf
Student <example@example.com>
2019-06-13 13:46:30 -0700

    fix form entry spacing and add placeholder

--- src/app/kegs/kegs.component.ts
+++ src/app/kegs/kegs.component.ts
@@ -2,7 +2,7 @@
 
 import { Component, Input } from '@angular/core';
 import { Keg } from '../models/keg.model';
-// import { $ } from 'protractor';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-kegs',
